08d7f8d615a02c15c1055997d3eaeb783ebf3ba0
Ali <alidadaashi@gmail.com>
2023-04-22 14:14:12 +0300

    Add result component & Implement it in app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { InputComponent } from './input/input.component';
+import { ResultComponent } from './result/result.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    InputComponent
+    InputComponent,
+    ResultComponent
   ],
   imports: [
     BrowserModule
