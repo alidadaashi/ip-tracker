0bf76e4f3002a4343385846ff434a531baa4425a
Ali <alidadaashi@gmail.com>
2023-04-22 14:41:55 +0300

    Add map component with dummy data & Implement it in app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { InputComponent } from './input/input.component';
 import { ResultComponent } from './result/result.component';
+import { MapComponent } from './map/map.component';
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 
 @NgModule({
   declarations: [
     AppComponent,
     InputComponent,
-    ResultComponent
+    ResultComponent,
+    MapComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    LeafletModule
   ],
   providers: [],
   bootstrap: [AppComponent]
