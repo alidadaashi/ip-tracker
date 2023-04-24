3e431e530fc3469b0b409be6ff285cfca5733498
Ali <alidadaashi@gmail.com>
2023-04-24 12:22:36 +0300

    Add Errorhandler to result service

--- src/app/common/services/result.service.spec.ts
+++ src/app/common/services/result.service.spec.ts
@@ -1,19 +1,40 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ResultService } from './result.service';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { CurrentIP } from '../models/currentIP';
 
 describe('ResultService', () => {
   let service: ResultService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-        imports:[HttpClientTestingModule]
+        imports:[HttpClientTestingModule],
+        providers:[ResultService]
     });
     service = TestBed.inject(ResultService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+
+  it('should return current IP address', () => {
+    const mockResponse: CurrentIP = { ip: '159.146.45.189' };
+ 
+    service.getIPAddress().subscribe(res => {
+      // expect(res.ip).toEqual(mockResponse.ip);
+      expect(res.ip).toMatch(/^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/)
+    });
+ 
+    const req = httpMock.expectOne('https://api.ipify.org?format=json');
+    expect(req.request.method).toBe('GET');
+    req.flush(mockResponse);
+  });
+
+
+  
 });
--- src/app/common/services/result.service.ts
+++ src/app/common/services/result.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
 import { Result } from '../models/result';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
 import { CurrentIP } from '../models/currentIP';
-
+import { catchError } from 'rxjs/operators';
+import { of } from 'rxjs';
 const BASE_URL = 'https://wookie.codesubmit.io/ipcheck?ip='
 
 @Injectable({
@@ -19,27 +20,31 @@ export class ResultService {
   };
   constructor(private http:HttpClient){}
   find(ip:string):Observable<Result>{
-    return this.http.get<Result>(this.getUrl(ip),this.httpOptions)
+    return this.http.get<Result>(this.getUrl(ip),this.httpOptions).pipe(
+      catchError(this.handleError))
   }
   private getUrl(ip:string):string{
     return `${BASE_URL}${ip}`
   }
-  getIPAddress(): Observable<CurrentIP> {
-    return this.http.get<CurrentIP>('https://api.ipify.org?format=json');
-  }
-  
-  initialResult: Result = {
-    ip: "192.212.174.101",
-    isp: "SpaceX- Starlink",
-    location: {
-      "city": "Mountain View",
-      "country": "US",
-      "geonameId": 5375480,
-      "lat": 37.38605,
-      "lng": -122.08385,
-      "postalCode": "94035",
-      "region": "California",
-      "timezone": "UTC-05:00"
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong.
+      console.error(
+        `Backend returned code ${error.status}, body was: `, error.message
+        );
+        alert("There is a problem. Please try again later")
     }
+    // Return an observable with a user-facing error message.
+    return throwError(() => new Error('Something bad happened; please try again later.'));
+  }
+
+  getIPAddress(): Observable<CurrentIP> {
+    return this.http.get<CurrentIP>('https://api.ipify.org?format=json').pipe(
+      catchError(this.handleError)
+    );
   }
 }
